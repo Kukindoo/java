73457d0e350a2099bea6ee50f90ba1cf77e4e5b7
Jakub Zavacky <Jakub.Zavacky@gmail.com>
2021-03-02 19:54:31 +0000

    Done

--- TDD_example1/src/StringHelper.java
+++ TDD_example1/src/StringHelper.java
@@ -1,4 +1,12 @@
-package main.java.com.ritmus.string;
-
 public class StringHelper {
+    public String swapLastTwoChars(String s){
+        int length = s.length();
+        if(length <= 1){
+            return s;
+        }
+        char firstChar = s.charAt(length-2);
+        char secondChar = s.charAt(length-1);
+        String subString = s.substring(0,length-2);
+        return subString + secondChar + firstChar;
+    }
 }
--- TDD_example1/src/StringHelperTest.java
+++ TDD_example1/src/StringHelperTest.java
@@ -1,12 +1,31 @@
-package test.java.com.ritmus.string;
-
-import main.java.com.ritmus.string.StringHelper;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class StringHelperTest {
+
+    StringHelper helper = new StringHelper();
     @Test
     public void testStringWith2CharsIsReversed(){
-        StringHelper helper = new StringHelper();
         assertEquals("BA",helper.swapLastTwoChars("AB"));
+        assertEquals("DC",helper.swapLastTwoChars("CD"));
+    }
+    @Test
+    public void testStringWith3CharsIsReversed(){
+        assertEquals("ACB",helper.swapLastTwoChars("ABC"));
+        assertEquals("CED",helper.swapLastTwoChars("CDE"));
+    }
+    @Test
+    public void testStringWith10CharsIsReversed(){
+        assertEquals("ABCDEFGHJI",helper.swapLastTwoChars("ABCDEFGHIJ"));
+        assertEquals("CED",helper.swapLastTwoChars("CDE"));
+    }
+    @Test
+    public void testStringWith1CharsIsReversed(){
+        assertEquals("A",helper.swapLastTwoChars("A"));
+    }
+    @Test
+    public void testStringWith0CharsIsReversed(){
+        assertEquals("",helper.swapLastTwoChars(""));
     }
 }
